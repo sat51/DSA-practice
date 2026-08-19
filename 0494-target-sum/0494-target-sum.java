630463be71dc54bd28ad3069bae7df09b6fbf93a
satyam tiwari <satyam432st@gmail.com>
2026-08-19 18:11:36 +0530

    Time: 7 ms (65.12%), Space: 46.7 MB (28.73%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,61 @@
 class Solution {
+
     public int findTargetSumWays(int[] nums, int target) {
-        // int [][] dp = new int[nums.length][];
-        return maxWays(nums,target,0);
+
+        int sum = 0;
+
+        for (int val : nums) {
+            sum += val;
+        }
+
+        if (target > sum || target < -sum) {
+            return 0;
+        }
+
+        Integer[][] dp = new Integer[nums.length][2 * sum + 1];
+
+        return maxWays(nums, target, 0, dp, sum);
     }
 
-    private int maxWays(int[] nums , int target, int currentIndex){
-        if(currentIndex == nums.length && target == 0)
-            return 1;
-        
-        if(currentIndex >= nums.length )
+    private int maxWays(
+            int[] nums,
+            int target,
+            int currentIndex,
+            Integer[][] dp,
+            int sum) {
+
+        if (currentIndex == nums.length) {
+            return target == 0 ? 1 : 0;
+        }
+
+        if (target > sum || target < -sum) {
             return 0;
+        }
 
-        int plusSign = maxWays(nums,target-nums[currentIndex],currentIndex+1);
-        int negativeSign = maxWays(nums, target+nums[currentIndex],currentIndex+1);
+        int updatedIndex = target + sum;
 
-        return plusSign + negativeSign;
+        if (dp[currentIndex][updatedIndex] != null) {
+            return dp[currentIndex][updatedIndex];
+        }
+
+        int plusSign = maxWays(
+                nums,
+                target - nums[currentIndex],
+                currentIndex + 1,
+                dp,
+                sum
+        );
+
+        int negativeSign = maxWays(
+                nums,
+                target + nums[currentIndex],
+                currentIndex + 1,
+                dp,
+                sum
+        );
+
+        dp[currentIndex][updatedIndex] = plusSign + negativeSign;
+
+        return dp[currentIndex][updatedIndex];
     }
 }
